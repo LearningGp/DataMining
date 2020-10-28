9d9c5d09d55d40535bb513c1d7b4111c68358b8e
LearningGp <arvinfield1998@outlook.com>
2020-10-28 16:23:14 +0800

    AprioriTest

---
+++ code/AprioriTest.py
@@ -0,0 +1,17 @@
+from efficient_apriori import apriori
+# 设置数据集
+data = [('尿布', '啤酒', '奶粉','洋葱'),
+('尿布', '啤酒', '奶粉','洋葱'),
+('尿布', '啤酒', '苹果','洋葱'),
+('尿布', '啤酒', '苹果'),
+('尿布', '啤酒', '奶粉'),
+('尿布', '啤酒', '奶粉'),
+('尿布', '啤酒', '苹果'),
+('尿布', '啤酒', '苹果'),
+('尿布', '奶粉', '洋葱'),
+('奶粉', '洋葱')
+]
+# 挖掘频繁项集和规则
+itemsets, rules = apriori(data, min_support=0.4, min_confidence=1)
+print(itemsets)
+print(rules)
